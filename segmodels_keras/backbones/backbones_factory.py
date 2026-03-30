8e4f8d8f5c1780b30d33d674556901eb5393e76b
theroggy <pieter.roggemans@gmail.com>
2026-03-30 17:26:04 +0200

    ENH: add some extra backbones

--- segmodels_keras/backbones/backbones_factory.py
+++ segmodels_keras/backbones/backbones_factory.py
@@ -47,6 +47,36 @@ class BackbonesFactory:
                 "conv1_relu",
             ),
         ),
+        "resnet50v2": (
+            ka.ResNet50V2,
+            ka.resnet_v2.preprocess_input,
+            (
+                "conv4_block6_1_relu",
+                "conv3_block4_1_relu",
+                "conv2_block3_1_relu",
+                "conv1_conv",
+            ),
+        ),
+        "resnet101v2": (
+            ka.ResNet101V2,
+            ka.resnet_v2.preprocess_input,
+            (
+                "conv4_block23_1_relu",
+                "conv3_block4_1_relu",
+                "conv2_block3_1_relu",
+                "conv1_conv",
+            ),
+        ),
+        "resnet152v2": (
+            ka.ResNet152V2,
+            ka.resnet_v2.preprocess_input,
+            (
+                "conv4_block36_1_relu",
+                "conv3_block8_1_relu",
+                "conv2_block3_1_relu",
+                "conv1_conv",
+            ),
+        ),
         # VGG
         "vgg16": (
             ka.vgg16.VGG16,
